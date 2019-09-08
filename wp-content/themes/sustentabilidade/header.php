8b4e6a6d08a098e50a8a889d522188266f653313
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-09-08 01:19:40 -0300

    Inicio de menu de navegacao

--- wp-content/themes/sustentabilidade/header.php
+++ wp-content/themes/sustentabilidade/header.php
@@ -4,3 +4,7 @@
 <script type="text/javascript">
 	var ajaxurl = "<?php echo admin_url( 'admin-ajax.php' ); ?>"
 </script>
+<nav>
+	<a href="<?php echo home_url(); ?>">HOME</a>
+	<a href="<?php echo get_post_type_archive_link('especie'); ?>">ESPECIES</a>
+</nav>
