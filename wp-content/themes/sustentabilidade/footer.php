6d9147c5e26d7e94d8d893a763e63af9d73d4d3b
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-09-17 20:31:04 -0300

    Header e footer com html

--- wp-content/themes/sustentabilidade/footer.php
+++ wp-content/themes/sustentabilidade/footer.php
@@ -1,3 +1,11 @@
-<?php 
-	echo 'PAGE FOOTER';
-	wp_footer();
+		<?php wp_footer(); ?>
+		<div class="footer">
+	  		<div class="container">
+			    <div>
+			      <a class="social-icon-link w-inline-block" href="#"><img src="http://uploads.webflow.com/560d79fc8d41e6275f31857f/560d79fc8d41e6275f3185b7_social-03.svg" width="20"></a><a class="social-icon-link w-inline-block" href="#"><img src="http://uploads.webflow.com/560d79fc8d41e6275f31857f/560d79fc8d41e6275f3185ce_social-18.svg" width="20"></a><a class="social-icon-link w-inline-block" href="#"><img src="http://uploads.webflow.com/560d79fc8d41e6275f31857f/560d79fc8d41e6275f3185aa_social-30.svg" width="20"></a>
+			    </div>
+	    		<div class="footer-text">Meu footer</div>
+	  		</div>
+		</div>
+	</body>
+</html>
