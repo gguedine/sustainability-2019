fea7dc6c04b4cdf58b221a96be8a07475021b14a
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-09-17 20:41:06 -0300

    Index php

--- wp-content/themes/sustentabilidade/index.php
+++ wp-content/themes/sustentabilidade/index.php
@@ -1,9 +1,28 @@
-<?php 
-
-get_header();
-
-echo "<br>";
-echo 'HOME PAGE SUSTENTABILIDADE'; 
-echo "<br>";
-
-get_footer();
+<?php get_header(); ?>
+<div class="body">
+  <div id="blog-post" class="blog-post view overlay">
+<!--     <a class="post-link" href="/blog-posts/skate-park" > -->
+      <img height="240" width="250" class="img-post center-block" src="http://www.pinhais.pr.gov.br/meioambiente/defesaanimal/dbimages/39728_img.png">
+    </a>
+    <div class="heading-wrapper">
+      <h1 class="blog-headline">Soaking in the colors</h1>
+      <div class="byline-wrapper">
+        <div class="byline-text">October 6, 2015</div>
+        <div class="byline-text">IN</div>
+        <a class="byline-link" href="#">Sports</a></div>
+    </div>
+    <p class="blog-content">After me I dragged six lengths of leather from
+            an equal number of my warriors. These lengths we had previously
+            fastened together, and passing one end to the topmost warrior I
+            lowered the other end cautiously over the opposite side of the wall
+            toward the avenue below. No one was in sight, so, lowering myself to
+            the end of my leather strap, I dropped the remaining thirty feet to
+            the pavement below.
+  </p>
+  <div class="middle">
+    <div class="text">John Doe</div>
+  </div>
+    <a class="link-to-page" href="#">Read More</a>
+  </div>
+</div>
+<?php get_footer(); ?>
