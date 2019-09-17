37c203116500ffabc54fc0eef03cf5b4c440e53a
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-09-17 20:31:29 -0300

    fix nos names dos inputs

--- wp-content/themes/sustentabilidade/page-login.php
+++ wp-content/themes/sustentabilidade/page-login.php
@@ -8,20 +8,10 @@
 	<form class = "form" method="post" action="<?php ?>">
 		<h2 class="form-head">Entrar</h2>
 		<label class="center-text-login">E-mail</label>
-		<input type="text" class="form-control" name="username" placeholder="E-mail" required="" autofocus="" />
+		<input type="text" class="form-control" name="login" placeholder="E-mail" required="" autofocus="" />
 		<label class="center-text-login">Senha</label>
-		<input type="password" class="form-control" name="password" placeholder="Senha" required=""/>      
-		<label class="checkbox">
-			<input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Esqueceu a senha?
-		</label>
+		<input type="password" class="form-control" name="senha" placeholder="Senha" required=""/>      
 		<button class="btn btn-lg btn-dark btn-block" type="submit">Logar</button>   
 	</form>
 </div>
 <?php get_footer(); ?>	
-
-<style>
-    body{
-        background-image: url("Animals-close-up-red-panda-rest-face-fence_1920x1080.jpg");
-        background-repeat:no-repeat;
-    }
-</style>
