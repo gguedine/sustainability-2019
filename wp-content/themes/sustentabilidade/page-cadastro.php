5ff0e3079eef7f49f6c28fb96a151850f3be6653
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-09-21 21:42:39 -0300

    Colocando classes no form

--- wp-content/themes/sustentabilidade/page-cadastro.php
+++ wp-content/themes/sustentabilidade/page-cadastro.php
@@ -1,16 +1,18 @@
 <?php do_action('do_cadastro'); ?>
 <?php get_header(); ?>
+<div class="box">
+	<h2 class="form-head">Cadastrar</h2>
+	<form class="form" method="post" action="<?php ?>">
+		<label class="center-text-login" for="login">Login</label>
+		<input class="form-control" type="text" name="login">
 
-<form method="post" action="<?php ?>">
-	<label for="login">Login</label>
-	<input type="text" name="login">
+		<label class="center-text-login" for="senha">Senha</label>
+		<input class="form-control" type="password" name="senha">
 
-	<label for="senha">Senha</label>
-	<input type="password" name="senha">
+		<input type="hidden" name="action" value="do_cadastro">
 
-	<input type="hidden" name="action" value="do_cadastro">
-
-	<button type="submit">Logar</button>
-</form>
+		<button class="btn btn-lg btn-dark btn-block" type="submit">Cadastrar</button>   
+	</form>
+</div>
 
 <?php get_footer(); ?>
