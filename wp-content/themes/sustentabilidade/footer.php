3cb1505cacf19dfde2389b28e519e3242bfae5c2
lipeflorentino <lipeflorentino2@gmail.com>
2019-09-23 20:00:02 -0300

    alteracoes no front das paginas

--- wp-content/themes/sustentabilidade/footer.php
+++ wp-content/themes/sustentabilidade/footer.php
@@ -4,7 +4,7 @@
 			    <div>
 			      <a class="social-icon-link w-inline-block" href="#"><img src="http://uploads.webflow.com/560d79fc8d41e6275f31857f/560d79fc8d41e6275f3185b7_social-03.svg" width="20"></a><a class="social-icon-link w-inline-block" href="#"><img src="http://uploads.webflow.com/560d79fc8d41e6275f31857f/560d79fc8d41e6275f3185ce_social-18.svg" width="20"></a><a class="social-icon-link w-inline-block" href="#"><img src="http://uploads.webflow.com/560d79fc8d41e6275f31857f/560d79fc8d41e6275f3185aa_social-30.svg" width="20"></a>
 			    </div>
-	    		<div class="footer-text">Meu footer</div>
+	    		<div class="footer-text">© Copyright 2019 - Sustainability</div>
 	  		</div>
 		</div>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
--- wp-content/themes/sustentabilidade/header.php
+++ wp-content/themes/sustentabilidade/header.php
@@ -4,7 +4,8 @@
   	<title>Sustentabilidade</title>
   	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">    
   	<link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet" type="text/css"> 
   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/style.css" />
