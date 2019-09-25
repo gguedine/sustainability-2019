05b4b6b6e3d8f3234bf2c5e1fa05dbe7d46ad88d
Rodrigo Calafate Cavote <rodrigocavote@id.uff.br>
2019-09-25 20:21:42 -0300

    Update archive-especie.php

--- wp-content/themes/sustentabilidade/archive-especie.php
+++ wp-content/themes/sustentabilidade/archive-especie.php
@@ -10,16 +10,16 @@
 			the_post();
 ?>
 
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img style="width: 458px; height: 343px" src="<?php echo get_field('imagem')['sizes']['large']; ?>">
-					<div class="caption">
-						<h3><?php the_title(); ?></h3>
-						<p><?php echo wp_trim_words(get_the_content(), 55, '...'); ?></p>
-				    	<p>
+			<div class="col-md-6 col-lg-4">
+				<div class="card mt-3 thumbnail">
+					<img class="card-img-top" src="<?php echo get_field('imagem')['sizes']['large']; ?>">
+					<div class="card-body caption">
+						<h3 class="card-title text-center"><?php the_title(); ?></h3>
+						<p class="card-text text-justify"><?php echo wp_trim_words(get_the_content(), 55, '...'); ?></p>
+				    	<div class="card-footer">
 				    		<!-- <a href="#" class="btn btn-primary" role="button">Button</a>  -->
-				    		<a href="<?php the_permalink(); ?>" class="btn btn-default" role="button">Saiba Mais...</a>
-				    	</p>
+				    		<a href="<?php the_permalink(); ?>" type="button" class="btn btn-outline-primary btn-sm">Saiba Mais...</a>
+				    	</div>
 			   		</div>
 				</div>
 			</div>
