4db35322d35bf09ffa0ad66b12af3d1543044621
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-09-25 23:32:04 -0300

    Precisa logar para registrar e mais um pouco de html

--- wp-content/themes/sustentabilidade/single-evento.php
+++ wp-content/themes/sustentabilidade/single-evento.php
@@ -5,18 +5,21 @@
 		the_post();
 ?>
 
-<?php 
-	if(!empty(get_the_category())): 
-		foreach (get_the_category() as $key => $category):
-?>
-	<span><?php echo $category->name; ?></span>
-<?php 
-		endforeach;
-	endif; 
-?>
 <div class="main-content">	
     <div class="box">
 		<h1><?php the_title(); ?></h1>	
+		<img src="<?php echo get_field('imagem')['sizes']['medium']; ?>">
+
+		<?php 
+			if(!empty(get_the_category())): 
+				foreach (get_the_category() as $key => $category):
+		?>
+			<span><?php echo $category->name; ?></span>
+		<?php 
+				endforeach;
+			endif; 
+		?>
+		<br><br>
 		<?php the_content(); ?>	
 		
 		<?php 
@@ -24,16 +27,20 @@
 			if(!empty($registrados)):
 				$registrado_key = array_search(get_current_user_ID(), array_column($registrados, 'ID'));
 				//retorna a chave do usuario no array ou false
+				echo '<h4>Usuários confirmados:</h4>';
 				foreach ($registrados as $key => $registrado_arr):
 					if(!($registrado_key === false)):
 		?>
-		<h3>Você já se registrou nesse evento</h3>
-		<?php else: ?>
-			<?php if(is_user_logged_in()): ?>
-			<button class="registrar-evento"  data-user="<?php echo get_current_user_ID(); ?>" data-evento="<?php echo get_the_ID(); ?>" >Registrar</button>
-			<?php endif; ?>
-		<?php endif ?>
-		<span><?php echo $registrado_arr['user_email']; ?></span><br>
+						<h3>Você já se registrou nesse evento</h3>
+					<?php else: ?>
+						<?php if(is_user_logged_in()): ?>
+							<button class="registrar-evento"  data-user="<?php echo get_current_user_ID(); ?>" data-evento="<?php echo get_the_ID(); ?>" >Registrar</button>
+						<?php else: ?>
+							<div class="alert alert-success" role="alert">Se logue para se registrar nesse evento</div>
+						<?php endif; ?>
+					<?php endif ?>
+
+				<span><?php echo $registrado_arr['user_email']; ?></span><br>
 
 		<?php 
 				endforeach;
@@ -43,6 +50,8 @@
 
 		<?php if(is_user_logged_in()): ?>
 			<button class="registrar-evento"  data-user="<?php echo get_current_user_ID(); ?>" data-evento="<?php echo get_the_ID(); ?>" >Registrar</button>
+		<?php else: ?>
+			<div class="alert alert-success" role="alert">Se logue para se registrar nesse evento</div>
 		<?php endif; ?>
 		<?php
 			endif;
@@ -51,7 +60,7 @@
 		<?php 
 			endif;
 		?>
-		</div>
-	</div>	
+	</div>
+</div>	
 
 <?php get_footer(); ?>
