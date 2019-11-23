891fb723f05a7c1d962421da636b46a36e3d0513
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-11-23 14:47:19 -0300

    Colocando abaixo assaindo na single iniciativa

--- wp-content/themes/sustentabilidade/single-iniciativa.php
+++ wp-content/themes/sustentabilidade/single-iniciativa.php
@@ -20,6 +20,28 @@
 				<?php endforeach; ?>
 		<?php endif; ?>
 	
+
+		<!-- ABAIXO ASSINADOS DA INICIATIVA -->
+		<?php 
+
+		$abaixo_assinados = get_posts(array(
+			'number_posts'=> -1,
+			'post_type'=> 'abaixo_assinado',
+		)); 
+
+		foreach ($abaixo_assinados as $key => $abaixo_assinado):
+			if(!in_array(get_the_ID(), get_field('iniciativa',$abaixo_assinado->ID)) ) continue;
+		?>
+
+		<div class="abaixo_assinado">
+			<a href="<?php print get_the_permalink($abaixo_assinado->ID); ?>">
+				<h2><?php print get_the_title($abaixo_assinado->ID); ?></h2>	
+			</a>
+			<span><?php print get_the_content($abaixo_assinado->ID); ?></span>
+		</div>
+
+		<?php endforeach ?>
+
 	</div>
 </body>
 
