e4cfbc00744059191da786c4bddc7831ab8a0c47
Alexandre Cardoso <alexandre.faria95@gmail.com>
2019-11-24 13:12:20 -0300

    Controle de acesso nas assinaturas

--- wp-content/themes/sustentabilidade/single-abaixo_assinado.php
+++ wp-content/themes/sustentabilidade/single-abaixo_assinado.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
 <body>
 	<div class="container">
+
+		<?php if(is_user_logged_in()): ?>
+			<button><a href="<?php print home_url('cria-abaixo-assinado'); ?>">Criar abaixo assinado</a></button>
+		<?php endif; ?>
+
 		<h1><?php the_title(); ?></h1>
 
 		<?php the_field('descricao'); ?>
@@ -10,9 +15,15 @@
 		<?php 
 			$assinantes = get_field('assinantes'); 
 			if(!$assinantes || (is_array($assinantes) && !in_array(get_current_user_ID(), $assinantes) )):
+				if(is_user_logged_in()):
 		?>
 			<button id="btn_assinar" data-uid="<?php print get_current_user_ID(); ?>" data-aid="<?php print get_the_ID(); ?>" >ASSINAR</button>
 		<?php
+				else:
+		?>
+			<a href="<?php print home_url('login') ?>">Faça Login para Assinar.</a>
+		<?php 
+				endif;
 			endif;
 		?>
 
